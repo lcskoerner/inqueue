dbf659bacf45ef9f6875c973618c115dc84c46d1
tanguydamois <tdamois@gmail.com>
2020-09-12 16:53:34 -0400

    correction again on the places table

--- db/migrate/20200912205143_add_coordinates_to_places.rb
+++ db/migrate/20200912205143_add_coordinates_to_places.rb
@@ -1,6 +1,6 @@
 class AddCoordinatesToPlaces < ActiveRecord::Migration[6.0]
   def change
     add_column :places, :latitude, :float
-    add_column :places, :longitute, :float
+    add_column :places, :longitude, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_204211) do
+ActiveRecord::Schema.define(version: 2020_09_12_205143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_09_12_204211) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
-    t.float "longitute"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
