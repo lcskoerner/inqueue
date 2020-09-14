ca9f670fe87810a9d7984658f1baffe870599e3d
romainbazeler <romain.bazeler@hotmail.com>
2020-09-13 22:08:51 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "places/search", to: "places#search", as: :places_search
+
+  resources :places, only: [ :show ]  do
+    resources :lines, only: [ :create, :show, :update, :destroy ]
+  end
 end
