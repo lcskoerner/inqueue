1676ee7e7a23434079c5f7891e3670a04e71d8e5
Lucas Koerner <lcskoerner@gmail.com>
2020-09-16 01:56:34 -0400

    autocomplete geolocated

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -3,6 +3,10 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ['keyword', 'places'];
 
+  connect() {
+    this.initAutoComplete();
+  }
+
   search() {
     const keyword = this.keywordTarget.value;
     const key = process.env.GOOGLE_API_KEY;
@@ -17,5 +21,32 @@ export default class extends Controller {
         });
       })
       .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
+  }
+
+  initAutoComplete() {
+    console.log(this.keywordTarget);
+    const autocomplete = new google.maps.places.Autocomplete(
+      this.keywordTarget
+    );
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const geolocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        const circle = new google.maps.Circle({
+          center: geolocation,
+          radius: position.coords.accuracy
+        });
+        autocomplete.setBounds(circle.getBounds());
+      });
+    }
+
+    google.maps.event.addDomListener(this.keywordTarget, 'keydown', function (e) {
+      if (e.key === "Enter") {
+        e.preventDefault(); // Do not submit the form on Enter.
+      }
+    });
   }
 }
