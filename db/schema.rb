f5c455cdbbd20caa41f59292fcee362d41ac6816
Lucas Koerner <lcskoerner@gmail.com>
2020-09-30 00:53:12 -0400

    add migration vicinity and phone number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_011319) do
+ActiveRecord::Schema.define(version: 2020_09_30_044604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2020_09_29_011319) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "last_line"
+    t.string "vicinity"
+    t.string "phone_number"
   end
 
   create_table "users", force: :cascade do |t|
